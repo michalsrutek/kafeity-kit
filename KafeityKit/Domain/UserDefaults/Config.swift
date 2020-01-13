ed8c5e904de850f9ae45c801f232cd1690244c0e
Libor Polehna <polehna.libor@gmail.com>
2020-01-13 13:36:44 +0400

    Updated ViewModel. Added UserDefaults.

---
+++ KafeityKit/Domain/UserDefaults/Config.swift
@@ -0,0 +1,9 @@
+//
+//  Config.swift
+//  KafeityKit
+//
+//  Created by Libor Polehna on 13/01/2020.
+//  Copyright © 2020 SKOUMAL, s.r.o. All rights reserved.
+//
+
+import Foundation
---
+++ KafeityKit/Domain/UserDefaults/UserDefault.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefault.swift
+//  KafeityKit
+//
+//  Created by Libor Polehna on 13/01/2020.
+//  Copyright © 2020 SKOUMAL, s.r.o. All rights reserved.
+//
+
+import Foundation
